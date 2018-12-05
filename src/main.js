89e2b3c1fcbb94dc16938ca576ad360eb5e3f621
sosky-yty <1027797811@qq.com>
2018-12-05 11:55:52 +0800

    下课了

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import '../static/weui/weui.css'
+import store from './store'
 Vue.config.productionTip = false
 App.mpType = 'app'
-
+// 注入store选项
+Vue.prototype.$store = store
 const app = new Vue(App)
 app.$mount()
